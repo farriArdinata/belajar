74735ea6f3a6b92aa69a3e2c46ae986e5ed9317c
farriArdinata <farryaldrinata660@gmail.com>
2022-07-05 15:14:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
+<?php
+$servername  = "localhost";
+$username = "root";
+$password = "";
+$database = "belajar_java_db";
 
-?>
+$sql = mysqli_connect($servername,$username,$password,$database);
+
+if (!sql) {
+  die("Koneksi Gagal");
+}
+
+ ?>
